78b498cdd8b248c4c59e360dfc4d8f1598c7559b
Kuppam Dheepesh Gupta <141455329+Dheepesh007@users.noreply.github.com>
2026-04-07 15:59:22 +0530

    Create session20_navigation.py

---
+++ session20_navigation.py
@@ -0,0 +1,13 @@
+# SESSION 20 – TESLA (Navigation)
+
+# Distance to goal (in meters)
+distance_to_goal = 0.15
+
+# Threshold for goal reaching
+threshold = 0.2
+
+# Check if goal is reached
+if distance_to_goal < threshold:
+    print("Goal Reached")
+else:
+    print("Goal Not Reached")
